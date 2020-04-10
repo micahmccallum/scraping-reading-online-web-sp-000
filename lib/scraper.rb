1dab1c6f94ec7aaee3b3133c6c5fa74c99902c67
Micah McCallum <micahmccallum@outlook.com>
2020-04-10 22:31:15 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,12 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+puts doc.css(".headline-26OIBN").text.strip
+courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .title-oE5vT4")
+
+courses.each do |course|
+  puts course.text.strip
+end
